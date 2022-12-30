9f4aa964081374bfa960e635a8dba6a73f2fc490
Leonardo Prado <leonardo.rprado@gmail.com>
2022-12-30 15:30:17 +0000

    feat: adicionando interface ao componente Post.tsx

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
@@ -8,7 +8,24 @@ import { Comment } from './Comment';
 
 import styles from './Post.module.css';
 
-export function Post({ author, publishedAt, content }) {
+interface Author {
+  avatarUrl: string;
+  name: string;
+  role: string;
+}
+
+interface Content {
+  type: 'paragraph' | 'link';
+  content: string;
+}
+
+interface PostProps {
+  author: Author;
+  publishedAt: Date;
+  content: Content[];
+}
+
+export function Post({ author, publishedAt, content }: PostProps) {
   const [comments, setComments] = useState(['Mandou muito mano!!! Parabens!', 'jdahsjdhagsdgasd 2545', 'laskufas nbsgd ghdgas 3234']);
   const [newCommentText, setNewCommentText] = useState('');
 
@@ -21,23 +38,23 @@ export function Post({ author, publishedAt, content }) {
     addSuffix: true,
   });
 
-  function handleCreateNewComment() {
+  function handleCreateNewComment(event: FormEvent) {
     event.preventDefault();
 
     setComments([...comments, newCommentText]);
     setNewCommentText('');
   };
 
-  function handleNewCommentChange() {
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
   };
 
-  function handleNewCommentInvalid() {
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity('comenta alguma coisa né camarada');
   }
 
-  function deleteComment(comment) {
+  function deleteComment(comment: string) {
     const commentsWhitoutDeletedOne = comments.filter(element => element !== comment);
     
     setComments(commentsWhitoutDeletedOne);
