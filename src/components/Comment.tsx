01c04be89bb12465f5d96804a2dc2ad6538c6c6e
Leonardo Prado <leonardo.rprado@gmail.com>
2022-12-30 15:38:05 +0000

    feat: adicionando interface ao componente Avatar.tsx

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -23,7 +23,7 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
   return (
     <div className={ styles.comment }>
-      <Avatar hasBorder={ false } src="https://avatars.githubusercontent.com/u/97796193?v=4"/ alt="" >
+      <Avatar hasBorder={ false } src="https://avatars.githubusercontent.com/u/97796193?v=4"  />
 
       <div className={ styles.commentBox}>
         <div className={ styles.commentContent }>
